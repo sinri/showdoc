fd0543583ca109febd6f457110fe4b3e84c4f370
Sinri Edogawa <e.joshua.s.e@gmail.com>
2016-12-12 10:04:13 +0800

    Add Register Leqee Auth API URL Config Item

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -6,4 +6,5 @@ return array(
     'DEFAULT_LANG' => 'zh-cn', // 默认语言
     'LANG_LIST'        => 'zh-cn,en-us', // 允许切换的语言列表 用逗号分隔
     'VAR_LANGUAGE'     => 'l', // 默认语言切换变量
+    'AA_URL'	=> 'https://yourapi.com/index.php',
 );
