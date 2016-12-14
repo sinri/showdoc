4e938215403ed39a84ba965421eaaa4f82bda05b
Sinri Edogawa <e.joshua.s.e@gmail.com>
2016-12-14 18:04:03 +0800

    upload again
    祝成功

--- Application/Home/Controller/PageController.class.php
+++ Application/Home/Controller/PageController.class.php
@@ -289,6 +289,70 @@ class PageController extends BaseController {
 
     // 大鲵的魔改
     public function upload(){
-        
+        $upload_dir=C("UPLOAD_DIR");
+        $file_name=uniqid().'_'. basename($_FILES['upload_file']['name']);
+        $uploadfile = $upload_dir .'/'.$file_name;
+
+        $uploadfile_max_size=30000;
+        if($_FILES['upload_file']['size']>$uploadfile_max_size){
+            echo "FILE Size: ".$_FILES['upload_file']['size']." OVER ".$uploadfile_max_size;
+            die()
+        }
+
+        if (move_uploaded_file($_FILES['upload_file']['tmp_name'], $uploadfile)) {
+            //well
+            echo "FILE LINK: ".$_SERVER['SCRIPT_URL'].'?s=/home/page/download&filename='.urlencode($file_name);
+        } else {
+            echo "<p>文件不正常……</p>";
+            print_r($_FILES);
+        }
+        exit();
     }
+
+    public function download(){
+        $file_name=I('filename');
+        $upload_dir=C("UPLOAD_DIR");
+        $uploadfile = $upload_dir .'/'.$file_name;
+        $done=$this->downloadFileAsName($uploadfile,$filename,$error);
+        if(!$done){
+            echo $error;
+        }
+    }
+
+    /**
+     * 文件下载
+    **/
+    private function downloadFileAsName($file, $down_name=null,&$error=''){
+        if($down_name!==null && $down_name!==false){
+            $suffix = substr($file,strrpos($file,'.')); //获取文件后缀
+            $down_name = $down_name.$suffix; //新文件名，就是下载后的名字
+        }else{
+            $k=pathinfo($file);
+            $down_name=$k['filename'].'.'.$k['extension'];
+        }
+
+        //判断给定的文件存在与否  
+        if(!file_exists($file)){
+            $error="您要下载的文件已不存在，可能是被删除";
+            return false;
+        }  
+        $fp = fopen($file,"r");
+        $file_size = filesize($file);
+        //下载文件需要用到的头
+        header("Content-type: application/octet-stream");
+        header("Accept-Ranges: bytes");
+        header("Accept-Length:".$file_size);
+        header("Content-Disposition: attachment; filename=".$down_name);
+        $buffer = 1024;
+        $file_count = 0;
+        //向浏览器返回数据  
+        while(!feof($fp) && $file_count < $file_size){
+            $file_con = fread($fp,$buffer);
+            $file_count += $buffer;
+            echo $file_con;
+        }  
+        fclose($fp);
+        return true;
+    }
+
 }
