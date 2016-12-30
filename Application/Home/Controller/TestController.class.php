81f36deb45c59c7b14699381e99dba5a9fec4f57
Sinri Edogawa <e.joshua.s.e@gmail.com>
2016-12-30 10:02:25 +0800

    Add test controller

---
+++ Application/Home/Controller/TestController.class.php
@@ -0,0 +1,24 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class TestController extends BaseController {
+    //项目列表页
+    public function index(){
+        $login_user = $this->checkLogin(); 
+		$this->show_debug_data('user_login',$login_user);
+
+        $items  = D("Item")->where("uid = '$login_user[uid]' or item_id in ( select item_id from ".C('DB_PREFIX')."item_member where uid = '$login_user[uid]' ) ")->select();
+        $this->show_debug_data("user's items",$items);
+
+        $items  = D("Item")->where("1=1");
+        $this->show_debug_data("all items",$items);
+    }
+
+    private function show_debug_data($title,$data){
+    	echo "<h3>".$title."</h3>".PHP_EOL;
+    	echo "<pre>";
+    	print_r($data);
+    	echo "</pre>";
+    	echo PHP_EOL;
+    }
+}
