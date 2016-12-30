19746c943db3e602bd8e92e5d7d26df64f1b7dbf
Sinri Edogawa <ljni@leqee.com>
2016-12-30 15:53:28 +0800

    digin

--- Application/Home/Controller/TestController.class.php
+++ Application/Home/Controller/TestController.class.php
@@ -34,4 +34,54 @@ class TestController extends BaseController {
             }
         }
     }
+
+    public function digin(){
+        $sql="SELECT 
+            item.item_id,item.item_name,item.item_description,
+            item.add_time,item.last_update_time,item.item_type,
+            item.uid,item.username
+        FROM item 
+        ";
+        $item_list=D()->query($sql);
+        $items=array();
+        if(!empty($item_list)){
+            foreach ($item_list as $item_index => $item) {
+                $items[$item['item_id']]=array(
+                    'item_id'=>$item['item_id'],
+                    'item_name'=>$item['item_name'],
+                    'item_description'=>$item['item_description'],
+                    'add_time'=>$item['add_time'],
+                    'last_update_time'=>$item['last_update_time'],
+                    'item_type'=>$item['item_type'],
+                    'owner_uid'=>$item['uid'],
+                    'owner_username'=>$item['username'],
+                    'members'=>array(
+                        $item['uid']=>array(
+                            'uid'=>$item['uid'],
+                            'username'=>$item['username'],
+                            'type'=>'owner',
+                            'member_group_id'=>-1,
+                        )   
+                    )
+                );
+                $sql="SELECT uid,username,member_group_id FROM Item_Member where item_id=".$item['item_id'];
+                $members=D()->query($sql);
+                if(!empty($members)){
+                    foreach ($members as $member_index => $member) {
+                        if(!isset($items[$item['item_id']]['members'][$member['uid']])){
+                            $items[$item['item_id']]['members'][$member['uid']]=array(
+                                $items[$item['item_id']]['members'][$member['uid']][$member['uid']]=array(
+                                    'uid'=>$member['uid'],
+                                    'username'=>$member['username'],
+                                    'type'=>'member',
+                                    'member_group_id'=>$member['member_group_id'],
+                                );
+                            );
+                        }
+                    }
+                }
+            }
+        }
+        print_r($items);
+    }
 }
